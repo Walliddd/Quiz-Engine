82bb4984b0363a37a7bd2bea8844795196f2b165
sajid <sajid.hossain2009@gmail.com>
2025-12-20 18:00:09 +0100

    feat: implement robust user input validation for quiz selection
    modified:   src/ui_terminal.py
    
    introduces the 'get_user_choice' function to
    handle reliably user selection during the quiz
    navigation phase.
    
    this function enforces strong validation checks:
    - non-empty input requirement.
    - conversion to integer, handling 'ValueError' for
    non-digit inputs.
    - range checking against the provided 'max_option'
    
    it ensures a smoother, less error-prone user
    experience by looping until valid input is
    provided, displaying context-aware error/warning
    messages for invalid entries

--- src/ui_terminal.py
+++ src/ui_terminal.py
@@ -1,6 +1,7 @@
 import os
 import platform
 from typing import List, Dict
+import string
 
 def print_welcome():
     pass
@@ -13,7 +14,22 @@ def get_user_choice(max_option):
     while True:
         clear_screen()
         print_header()
-        print(f"Scegli un'opzione tra ")
+
+        selection_str = input(f"\nSelect a quiz (1 - {max_option}): ").strip()
+
+        if not selection:
+            print("[WARN] Input cannot be empty. Please try again.")
+            continue
+
+        try:
+            selection = int(selection_str)
+
+            if selection >= 1 and selection <= max_option:
+                return selection
+            else:
+                print(f"[ERROR] Your choice is out of range.")
+        except ValueError:
+            print(f"[ERROR] '{selection_str}' is not a valid integer. Please enter only digits.")
 
 def clear_screen():
     if platform.system() == "Windows":
@@ -23,7 +39,7 @@ def clear_screen():
         os.system("clear")
 
 def print_header():
-    header = """ # ascii art font name 'Classy'
+    header = """ # ascii art font name: 'Classy'
                                                                   
    ▄▄▄▄                         ▄▄▄▄▄▄▄                           
  ▄█▀▀███▄▄                     █▀██▀▀▀                            
@@ -34,5 +50,6 @@ def print_header():
        ▀█                                        ██               
                                                ▀▀▀                """
 
-def display_question():
-    pass 
+def display_question(question_data: Dict, question_number: int, total_questions: int):
+    print("-" * 35)
+    print(f"")
