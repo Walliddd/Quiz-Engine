5b7136afbf67cc886621a9e4691ae57e5f2f7e68
sajid <sajid.hossain2009@gmail.com>
2025-12-21 10:54:44 +0100

    refactor: finalize quiz interaction loop in run_quiz
    modified:   src/engine.py
    
    completed the main loop for iterating through
    questions, getting user input, checking
    correctness, and updating the match status
    (score, correct/incorrect counts)

--- src/engine.py
+++ src/engine.py
@@ -1,4 +1,5 @@
-from src.ui_terminal import display_question, get_answer
+from src.ui_terminal import display_question, get_answer, clear_screen
+import string
 
 def run_quiz(quiz_data):
     match_status = {
@@ -16,8 +17,32 @@ def run_quiz(quiz_data):
     for question in quiz_data["questions"]:
         display_question(question)
 
+        option_list = question["options"]
         user_answer = get_answer()
         correct_answer = question["correctOption"]
 
-        
+        letter_to_index = {}
 
+        for index in range(len(option_list)):
+            if index < len(string.ascii_uppercase):
+                letter = string.ascii_uppercase[index]
+                letter_to_index[letter] = index
+            else:
+                break
+
+        user_index = letter_to_index.get(user_answer, -1)
+
+        if user_index == correct_answer:
+            match_status["correct_answers"] += 1
+            points = question["points"]
+            match_status["score"] += points
+
+            print(f"\nCongratulations! Your answer is correct! \nExplanation: {question["explanation"]}")
+        else:
+            match_status["incorrect_answers"] += 1
+            penalty = question.get("penalty", 0)
+            match_status["score"] -= penalty
+
+            print(f"Too bad! Your answer wasn't correct! It was the option '{option_list[correct_answer]}'")
+
+def show_results():
