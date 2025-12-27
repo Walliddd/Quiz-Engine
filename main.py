a2482bf4cdd0d21e5e76a7f53f482d69445a8697
sajid <sajid.hossain2009@gmail.com>
2025-12-27 22:48:06 +0100

    feat: initialize main entry point and display main menu
    modified:   main.py
    
    sets up the basic structure of main.py by
    importing necessary modules and rendering the
    initial main menu screen using UI functions

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from src.data_manager import load_quiz_file, load_quiz_data
-from src.creator import save_new_quiz
+import sys 
+from src.creator import run_quiz_creator
+from src.engine import run_quiz
+from src.data_manager import load_quiz_file, ensure_data_directory, load_quiz_data
+from src.storage import load_leaderboard, save_leaderboard, display_top_10
+from src.ui_terminal import clear_screen, print_header, get_username
+from src.colors import color_blue, color_red, color_yellow
 
-save_new_quiz()
+
+def display_main_menu():
+    clear_screen()
+    print_header()
+    print(color_blue("\n" + "=" * 35))
+    print(color_yellow("      MAIN MENU"))
+    print(color_blue("=" * 35))
+    print("1. Create a New Quiz")
+    print("2. Play a Quiz")
+    print("3. Exit application")
+    print(color_blue("-" * 35))
 
 files = load_quiz_file()
 
